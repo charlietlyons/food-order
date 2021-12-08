6af67d89faa260cdad24a91e79a7f524c55e5fb3
Charlie Lyons <charlietlyons@gmail.com>
2021-12-08 11:35:50 -0600

    Additional modal styling

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,18 +1,22 @@
 import React, { useContext } from 'react';
 import Column from '../Common/Column'
+import Row from '../Common/Row';
 import Button from '../Button/Button'
+import CartContext from '../../context/CartContext';
 
 import styles from './Cart.module.css'
-import CartContext from '../../context/CartContext';
 
 const CartItem = (props) => {
     const cartContext = useContext(CartContext)
 
     return <div className={styles.cartItem}>
         <Column>
-            <p>{props.item.title}</p>
-            <p>${props.item.price}</p>
-            <p>x{props.item.amount}</p>
+            <p className={styles.itemTitle}>{props.item.title}</p>
+            <Row className={styles.purchaseInfo}>
+                <p className={styles.priceText}>${props.item.price}</p>
+                <p>x</p>
+                <p className={styles.amountText}>{props.item.amount}</p>
+            </Row>
         </Column>
         <Column className={styles.cartItemInputs}>
             <Button className={styles.plusButton} onClick={() => cartContext.incrementItem(props.item)}>+</Button>
